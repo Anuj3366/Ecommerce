24692c30328e3a5ce90acfb471d1fa561eb86ede
Anuj3366 <anujgarg3366@gmail.com>
2024-12-12 17:43:36 +0530

    error solved

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 const cors = require("cors")
 require("dotenv").config()
-const connectDB = require("./config/d")
+const connectDB = require("./config/db")
 const PORT = process.env.PORT || 5000
 
 // middlewares
