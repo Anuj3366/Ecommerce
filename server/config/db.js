836bdd846d581d41cbfa9ae78c84b542e08ee0e7
Anuj3366 <anujgarg3366@gmail.com>
2024-12-27 15:04:53 +0530

    Enhance MongoDB connection settings in db.js to include new options for improved stability and error handling

--- server/config/db.js
+++ server/config/db.js
@@ -2,10 +2,14 @@ const mongoose = require("mongoose")
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI)
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            serverSelectionTimeoutMS: 30000
+        })
         console.log("Mongo DB Connected: ", conn.connection.host)
     }
-    catch(err) {
+    catch (err) {
         console.log(err)
         process.exit(1)
     }
